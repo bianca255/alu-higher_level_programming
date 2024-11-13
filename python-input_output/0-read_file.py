44e0dd2c38b8e4aacfc1a37327c737e3781fb4dd
bianca255 <b.haguma@alustudent.com>
2024-11-13 06:00:33 -0800

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
+"""Defines a text file-reading function."""
 
-a = 1
-b = 2
 
-if __name__ == '__main__':
-    print("{} + {} = {}".format(a, b, add(a, b)))
+def read_file(filename=""):
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
